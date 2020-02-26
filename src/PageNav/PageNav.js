d4cbf195378b7a1f796db8db2fe81344a31f3fcf
Michael Sanders <mikesanderspsu@gmail.com>
2020-02-25 22:22:08 -0500

    Fixed errors in file

--- src/PageNav/PageNav.js
+++ src/PageNav/PageNav.js
@@ -1,35 +1,32 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Circle from '../Circle/Circle'
-import NotesContext from '../note-content'
 import './PageNav.css'
 
-class PageNav extends Component {
-
-  static contextType = NotesContext
-
-  render() {
-    const {folders} = this.context
+export default function PageNav (props) {
     return (
       <div className='PageNav'>
         <Circle
           tag='button'
           role='link'
-          onClick={() => this.props.history.goBack()}
+          onClick={() => props.history.goBack()}
           className='PageNavBack'
         >
           <FontAwesomeIcon icon='chevron-left' />
           <br />
           Back
         </Circle>
-        {folders && (
+        {props.folders && (
           <h3 className='PageNavFolder'>
-            {folders.name}
+            {props.folders.name}
           </h3>
         )}
       </div>
     )
-      }
+      
 }
-
-export default PageNav
+PageNav.defaultProps = {
+  history: {
+    goBack: () => {}
+  }
+}
