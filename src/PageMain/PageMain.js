53ae3f01d93afce10135519d5dd9a86c14516bf0
Michael Sanders <mikesanderspsu@gmail.com>
2020-02-25 22:20:21 -0500

    Fixed errors in file

--- src/PageMain/PageMain.js
+++ src/PageMain/PageMain.js
@@ -2,27 +2,30 @@ import React from 'react'
 import Note from '../Note/Node'
 import './PageMain.css'
 
-export default function PageMain(props) {
-  return (
-    <section className='PageMain'>
+export default class PageMain extends React.Component {
+  
+  render () {    
+    return (
+      <section className='PageMain'>
       <Note
-        id={props.note.id}
-        name={props.note.name}
-        modified={props.note.modified}
-        history={props.history}
-        match={props.match}
+        id={this.props.note.id}
+        name={this.props.note.name}
+        modified={this.props.note.modified}
+        handleDeleteNote={() => this.props.history.push('/')}
       />
       <div className='PageMainContent'>
-        {props.note.content.split(/\n \r|\n/).map((para, i) =>
+        {this.props.note.content.split(/\n \r|\n/).map((para, i) =>
           <p key={i}>{para}</p>
         )}
       </div>
     </section>
   )
 }
+}
 
 PageMain.defaultProps = {
   note: {
     content: '',
   }
+
 }
