c57441d58b37adec5e4bece13030b8eadeb19bf9
Michael Sanders <mikesanderspsu@gmail.com>
2020-02-25 22:21:27 -0500

    Fixed error when changeing pages. However Setstate error still occurs

--- src/ListNav/ListNav.js
+++ src/ListNav/ListNav.js
@@ -3,47 +3,43 @@ import { NavLink, Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Circle from '../Circle/Circle'
 import { countNotesForFolder } from '../notes-help'
-import NotesContext from '../note-content';
+import NotesContent from '../note-content';
 import './ListNav.css'
 
-class ListNav extends Component {
-  
-  static contextType = NotesContext
-
-  render(){
-    const {notes, folders} = this.context;
-    return (
-      <div className='ListNav'>
-        <ul className='ListNavList'>
-          {folders.map(folder =>
+export default function ListNav () {
+  return (
+    <NotesContent.Consumer>
+      {({folders, notes}) => {
+       return (<div className='ListNav'>
+       <ul className='ListNavList'>
+           {folders.map(folder =>
             <li key={folder.id}>
-              <NavLink
+               <NavLink
                 className='ListNavFolder'
-                to={`/folder/${folder.id}`}
-              >
-                <span className='NoteListNavNotes'>
-                  {countNotesForFolder(notes, folder.id)}
-                </span>
-                {folder.name}
-              </NavLink>
-            </li>
-          )}
-        </ul>
-        <div className='NoteListNavWrapper'>
-          <Circle
-            tag={Link}
-            to='/add-folder'
-            type='button'
-            className='ListNavAddFolder'
-          >
-            <FontAwesomeIcon icon='plus' />
-            <br />
-            Folder
-          </Circle>
-        </div>
-      </div>
+                 to={`/folders/${folder.id}`}
+                >
+             <span className='NoteListNavNotes'>
+               {countNotesForFolder(notes, folder.id)}
+             </span>
+             {folder.name}
+           </NavLink>
+         </li>
+       )}
+     </ul>
+     <div className='ListNavWrapper'>
+       <Circle
+         tag={Link}
+         to='/add-folder'
+         type='button'
+         className='ListNavAddFolder'
+       >
+         <FontAwesomeIcon icon='plus' />
+         <br />
+         Folder
+       </Circle>
+     </div>
+   </div>) 
+}}
+    </NotesContent.Consumer>
     )
-  }
-}
-
-export default ListNav
+ }
